4ae44cc6f1e5fb82be55efbce878d5b49a9dcc2f
Logan Ricketts <lricketts@unm.edu>
2024-12-03 19:04:43 -0700

    added more to admin

--- Main/Admin.java
+++ Main/Admin.java
@@ -1,9 +1,19 @@
 package Main;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Admin {
 
-    public Admin(String cardCode) {
-        System.out.println("New admin... card code: " + cardCode);
+    private String adminID;
+    private ArrayList<Latch> latches = new ArrayList<>();
+    private boolean adminDone;
+
+    public Admin(String cardCode, ArrayList<Latch> latches) {
+        adminID = cardCode;
+        latches.addAll(this.latches);
+        System.out.println("New admin... card code: " + adminID);
     }
 
     public void startAdminThread() {
@@ -18,5 +28,45 @@ public class Admin {
                 }
             }
         });
+    }
+
+    public void openVoting(){
+
+    }
+
+    public void closeVoting(){
+
+    }
+
+    public void openSession(){
+
+    }
+
+    public void closeSession(){
+
+    }
+
+    public void shutdown(){
+
+    }
+
+    public void openLatches(){
+        for(Latch latch : latches) {
+            latch.unlockLatch();
+        }
+    }
+
+    public void closeLatches(){
+        for(Latch latch : latches) {
+            latch.lockLatch();
+        }
+    }
+
+    public void confirmShutdown(){
+
+    }
+
+    public boolean isAdminDone() {
+        return adminDone;
     }
 }
